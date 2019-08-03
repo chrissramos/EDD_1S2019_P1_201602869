25684d7cb5506f711ecc7485e8f1c40e26eb4bfa
chrissramos <3492193460115@ingenieria.usac.edu.gt>
2019-08-03 02:14:41 -0600

    agregando Cola

--- Structures/Cola.py
+++ Structures/Cola.py
@@ -15,10 +15,29 @@ class Cola:
             temp = self.head
             while temp.next is not None:
                 temp = temp.next
-            temp.next = node 
+            temp.next = node
+
+    def print_list(self):
+        if self.head is None:
+            print("cola vacia")
+        else:
+            temp = self.head
+            while temp.next is not None:
+                print("[", temp.name, ",", temp.score, "]", end='')
+                print("->", end='')
+                temp = temp.next
+            print("[", temp.name, ",", temp.score, "]","->NULL")
 
 
 
+cola = Cola()
+cola.add(Node("randall",100))
+cola.add(Node("lucas",20))
+cola.add(Node("pedro",30))
+cola.add(Node("luis",25))
+cola.add(Node("Manola",200))
+cola.add(Node("Colocha",2000))
 
+cola.print_list()
 
 input()
